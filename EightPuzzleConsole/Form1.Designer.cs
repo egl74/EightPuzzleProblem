943102510d0c1eba2de3e5287b863ed6d2c8205a
egl74 <laptenauck@gmail.com>
2015-04-30 20:22:13 +0300

    Redesigned in Windows Forms

---
+++ EightPuzzleConsole/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+
+namespace EightPuzzleConsole
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.Format = "D0";
+            dataGridViewCellStyle4.NullValue = null;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.Location = new System.Drawing.Point(116, 88);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Old English Text MT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.RowTemplate.Height = 99;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(300, 300);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Old English Text MT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.Frozen = true;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MaxInputLength = 1;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 99;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Old English Text MT", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MaxInputLength = 1;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 99;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Old English Text MT", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column3.Frozen = true;
+            this.Column3.HeaderText = "Column3";
+            this.Column3.MaxInputLength = 1;
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.Width = 99;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 436);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "8-puzzle";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
---
+++ EightPuzzleConsole/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using EightPuzzleGame;
+using EightPuzzleGame.Calculators;
+using EightPuzzleGame.SuccessorNodes;
+
+namespace EightPuzzleConsole
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.Add(8, 2, null);
+            dataGridView1.Rows.Add(1, 5, 3);
+            dataGridView1.Rows.Add(4, 6, 7);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.DoubleBuffer, true);
+
+
+            //dataGridView1.Rows.Add(1, 2, 3);
+            //dataGridView1.Rows.Add(4, 5, 6);
+            //dataGridView1.Rows.Add(7, null, 8);
+
+        }
+
+        private void Calculate(EightPuzzleNode start)
+        {
+            try
+            {
+                dataGridView1.ClearSelection();
+                //do
+                //{
+                var goal = new EightPuzzleNode {Tiles = new[] {1, 2, 3, 4, 5, 6, 7, 8, 0}};
+                //var start = new EightPuzzleNode {Tiles = new int[9]};
+
+                //Console.WriteLine("Enter a valid start state (e.g. 867254031) - numbers '1'-'8' + marker '0'");
+                //string userinput = Console.ReadLine();
+
+
+                //if (userinput != null)
+                //{
+                //int i = 0;
+
+                //foreach (char s in userinput)
+                //{
+                //    int tile;
+
+
+                //    if (!int.TryParse(s.ToString(), out tile)) continue;
+
+                //    start.Tiles[i++] = tile;
+                //}
+
+                var pathFinder = new PathFinder(
+                    new EightPuzzleSuccessorNodesGenerator(),
+                    new EightPuzzleGValueCalculator(),
+                    new EightPuzzleManhattanDistanceCalulator());
+
+                INode result = pathFinder.Execute(start, goal);
+                PrintSolution(result, dataGridView1);
+
+                // Console.ReadKey();
+                //}
+                //} while (Console.ReadLine() != "exit");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private static void PrintSolution(INode node, DataGridView dg)
+        {
+
+            if (node != null)
+            {
+                var stack = new Stack<INode>();
+
+                do
+                {
+                    stack.Push(node);
+                } while ((node = node.Parent) != null);
+
+                EightPuzzleNode check = (EightPuzzleNode) stack.First();
+                if (check.Tiles.SequenceEqual(new[] {1, 2, 3, 4, 5, 6, 7, 8, 0}))
+                {
+                    MessageBox.Show("No need to solve solved puzzle");
+                    return;
+                }
+
+                //var step = 0;
+
+                foreach (EightPuzzleNode solutionNode in stack)
+                {
+
+                    dg.EditMode = DataGridViewEditMode.EditProgrammatically;
+
+                    //dg.SelectAll();
+
+                    //Console.Clear();
+
+                    //Console.WriteLine("");
+
+                    string tiles = solutionNode.Tiles
+                        .Aggregate("", (current, i) => current + i.ToString());
+
+                    for (var i = 0; i < 9; i++)
+                    {
+                        
+                        var col = (i)%3;
+                        var row = (i)/3;
+                        dg.BeginEdit(true);
+                        dg.NotifyCurrentCellDirty(false);
+                        dg[col, row].Value = tiles[i] != '0' ? (object) (int) Char.GetNumericValue(tiles[i]) : null;
+                        dg.EndEdit();
+                        dg.NotifyCurrentCellDirty(false);
+
+
+                        //dg[i, j].Value = tiles[i];
+                        //Console.WriteLine("  {0} {1} {2}",
+                        //    tiles[i] == '0' ? '_' : tiles[i],
+                        //    tiles[i + 1] == '0' ? '_' : tiles[i + 1],
+                        //    tiles[i + 2] == '0' ? '_' : tiles[i + 2]);
+                        //i = i + 2;
+
+                    }
+
+                    //Thread.Sleep(500);
+                    new ManualResetEvent(false).WaitOne(500);
+                    //step++;
+                    dg.EditMode = DataGridViewEditMode.EditOnKeystroke;
+                }
+
+
+                MessageBox.Show(stack.Count - 1 + " steps");
+            }
+            else
+            {
+                MessageBox.Show("No solution");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int[] column0Array = new int[dataGridView1.Rows.Count];
+            //int[] column1Array = new int[dataGridView1.Rows.Count];
+            //int[] column2Array = new int[dataGridView1.Rows.Count];
+
+            //int i = 0;
+
+            //foreach (DataGridViewRow row in dataGridView1.Rows)
+            //{
+
+            //    column0Array[i] = row.Cells[0].Value != null ? Convert.ToInt32(row.Cells[0].Value) : 0;
+            //    column1Array[i] = row.Cells[1].Value != null ? Convert.ToInt32(row.Cells[1].Value) : 0;
+            //    column2Array[i] = row.Cells[2].Value != null ? Convert.ToInt32(row.Cells[2].Value) : 0;
+            //    i++;
+            //}
+
+            //var array = new int[column0Array.Length + column1Array.Length + column2Array.Length];
+            //column0Array.CopyTo(array, 0);
+            //column1Array.CopyTo(array, column1Array.Length);
+            //column2Array.CopyTo(array, column1Array.Length + column2Array.Length);
+            //column0Array
+            var array = new int[9];
+            for (var i = 0; i < dataGridView1.Rows.Count; i++)
+                for (var j = 0; j < dataGridView1.Columns.Count; j++)
+                    array[3*j + i] = dataGridView1[i, j].Value != "" ? Convert.ToInt32(dataGridView1[i, j].Value) : 0;
+            Calculate(new EightPuzzleNode {Tiles = array});
+            //Calculate(new EightPuzzleNode {Tiles = new [] {2,8,7,4,3,0,5,6,1}});
+        }
+    }
+}
