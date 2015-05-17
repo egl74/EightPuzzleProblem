974692a5a8b8693cd6cd60da590dfc5e336cee1a
egl74 <laptenauck@gmail.com>
2015-05-17 18:32:53 +0300

    removed commented out code lines

--- EightPuzzleConsole/Form1.cs
+++ EightPuzzleConsole/Form1.cs
@@ -23,12 +23,6 @@ namespace EightPuzzleConsole
             SetStyle(ControlStyles.UserPaint, true);
             SetStyle(ControlStyles.AllPaintingInWmPaint, true);
             SetStyle(ControlStyles.DoubleBuffer, true);
-
-
-            //dataGridView1.Rows.Add(1, 2, 3);
-            //dataGridView1.Rows.Add(4, 5, 6);
-            //dataGridView1.Rows.Add(7, null, 8);
-
         }
 
         private void Calculate(EightPuzzleNode start)
@@ -85,13 +79,7 @@ namespace EightPuzzleConsole
                 {
 
                     dg.EditMode = DataGridViewEditMode.EditProgrammatically;
-
-                    //dg.SelectAll();
-
-                    //Console.Clear();
-
-                    //Console.WriteLine("");
-
+                    
                     string tiles = solutionNode.Tiles
                         .Aggregate("", (current, i) => current + i.ToString());
 
@@ -105,23 +93,11 @@ namespace EightPuzzleConsole
                         dg[col, row].Value = tiles[i] != '0' ? (object) (int) Char.GetNumericValue(tiles[i]) : null;
                         dg.EndEdit();
                         dg.NotifyCurrentCellDirty(false);
-
-
-                        //dg[i, j].Value = tiles[i];
-                        //Console.WriteLine("  {0} {1} {2}",
-                        //    tiles[i] == '0' ? '_' : tiles[i],
-                        //    tiles[i + 1] == '0' ? '_' : tiles[i + 1],
-                        //    tiles[i + 2] == '0' ? '_' : tiles[i + 2]);
-                        //i = i + 2;
-
                     }
 
-                    //Thread.Sleep(500);
                     new ManualResetEvent(false).WaitOne(500);
-                    //step++;
                     dg.EditMode = DataGridViewEditMode.EditOnKeystroke;
                 }
-
 
                 MessageBox.Show(stack.Count - 1 + " steps");
             }
@@ -134,32 +110,11 @@ namespace EightPuzzleConsole
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //int[] column0Array = new int[dataGridView1.Rows.Count];
-            //int[] column1Array = new int[dataGridView1.Rows.Count];
-            //int[] column2Array = new int[dataGridView1.Rows.Count];
-
-            //int i = 0;
-
-            //foreach (DataGridViewRow row in dataGridView1.Rows)
-            //{
-
-            //    column0Array[i] = row.Cells[0].Value != null ? Convert.ToInt32(row.Cells[0].Value) : 0;
-            //    column1Array[i] = row.Cells[1].Value != null ? Convert.ToInt32(row.Cells[1].Value) : 0;
-            //    column2Array[i] = row.Cells[2].Value != null ? Convert.ToInt32(row.Cells[2].Value) : 0;
-            //    i++;
-            //}
-
-            //var array = new int[column0Array.Length + column1Array.Length + column2Array.Length];
-            //column0Array.CopyTo(array, 0);
-            //column1Array.CopyTo(array, column1Array.Length);
-            //column2Array.CopyTo(array, column1Array.Length + column2Array.Length);
-            //column0Array
             var array = new int[9];
             for (var i = 0; i < dataGridView1.Rows.Count; i++)
                 for (var j = 0; j < dataGridView1.Columns.Count; j++)
                     array[3*j + i] = dataGridView1[i, j].Value != "" ? Convert.ToInt32(dataGridView1[i, j].Value) : 0;
             Calculate(new EightPuzzleNode {Tiles = array});
-            //Calculate(new EightPuzzleNode {Tiles = new [] {2,8,7,4,3,0,5,6,1}});
         }
     }
 }
